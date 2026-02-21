f4843260581f90ddd9ce6f8322cb6c2ceba25451
Grifzilla <griffinkinley@protonmail.com>
2026-02-21 15:08:34 -0600

    removed plugins, adjusted keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,42 +1,53 @@
--- Simple settings
+-- Basic settings
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
 vim.o.signcolumn = "yes"
-vim.o.tabstop = 2
-vim.o.swapfile = false
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 8
 vim.o.cursorline = true
+
+-- Indentation
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.swapfile = false
 
 -- Vim keybinds
 vim.g.mapleader = " "
-vim.keymap.set('n', '<leader>rl', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>rc', ':e $MYVIMRC<CR> :source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>rl', ':update<CR> :source<CR>', { desc = "Source" })
+vim.keymap.set('n', '<leader>rc', ':e $MYVIMRC<CR> :source<CR>', { desc = "Open Init.lua" })
+vim.keymap.set('n', '<leader>w', ':write<CR>', { desc = "Write" })
+vim.keymap.set('n', '<leader>q', ':quit<CR>', { desc = "Quit" })
 
-vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y<CR>')
-vim.keymap.set({'n', 'v', 'x'}, '<leader>d', '"+d<CR>')
-vim.keymap.set('i', 'jj', '<Esc>', { noremap = true })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y<CR>', { desc = "Yank to paste buffer" })
+vim.keymap.set({'n', 'v', 'x'}, '<leader>d', '"+d<CR>', { desc = "Delete to paste buffer" })
+vim.keymap.set('i', 'jj', '<Esc>', { noremap = true }, { desc = "jj mapped to Escape" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true }, { desc = "Center on page down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true }, { desc = "Center on page up" })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = "Center on next" })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = "Center on previous" })
+
+vim.keymap.set('n', '<A-j>', ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set('n', '<A-k>', ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = "Move line down" })
+
+vim.keymap.set('n', '<leader>e', ":Explore<CR>", { desc = "Open file explorer" })
 
 --Packages
 vim.pack.add({
 		{ src = "https://github.com/catppuccin/nvim.git" },
-		{ src = "https://github.com/stevearc/oil.nvim" },
-		{ src = "https://github.com/nvim-mini/mini.pick" },
 })
-
-require "mini.pick".setup()
-require "oil".setup()
-
--- Package keybinds
-vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
-vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
-vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
 --Appearance
 vim.cmd("colorscheme catppuccin")
 vim.o.showmatch = true
+
+-- Functions
+vim.api.nvim_create_autocmd("TextYankPost", { -- Highlight on yank
+		callback = function()
+				vim.highlight.on_yank()
+		end,
+}) 
